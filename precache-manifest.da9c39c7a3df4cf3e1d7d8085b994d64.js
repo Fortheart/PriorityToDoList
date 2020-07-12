e0720a1442c84939be019f1d92cdb4c58afcdc64
Martin Zvozil <martin.zvozil@gmail.com>
2020-07-12 22:58:57 +0200

    Updates

---
+++ precache-manifest.da9c39c7a3df4cf3e1d7d8085b994d64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39aa6a4f16bd5b1a1db0c6f16632fa66",
+    "url": "/PriorityToDoList/index.html"
+  },
+  {
+    "revision": "e50d1855f420cb5573aa",
+    "url": "/PriorityToDoList/static/css/main.d528fc9d.chunk.css"
+  },
+  {
+    "revision": "5fd66ba24cccd4929046",
+    "url": "/PriorityToDoList/static/js/2.91413049.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PriorityToDoList/static/js/2.91413049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50d1855f420cb5573aa",
+    "url": "/PriorityToDoList/static/js/main.5daae258.chunk.js"
+  },
+  {
+    "revision": "e1a8bddc690497fe3a36",
+    "url": "/PriorityToDoList/static/js/runtime-main.2e6defec.js"
+  },
+  {
+    "revision": "1f713fac6b68ace3ce265562ad98bbe9",
+    "url": "/PriorityToDoList/static/media/check-solid.1f713fac.svg"
+  },
+  {
+    "revision": "aa97bc0003f01006130176127ea617f4",
+    "url": "/PriorityToDoList/static/media/times-solid.aa97bc00.svg"
+  }
+]);
